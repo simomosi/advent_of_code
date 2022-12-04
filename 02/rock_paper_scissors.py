53bdcc5fc565884e5733bfc4a9b7269c2d4e4898
simomosi <37104045+simomosi@users.noreply.github.com>
2022-12-04 14:30:16 +0000

    Refactoring, added types

--- 02/rock_paper_scissors.py
+++ 02/rock_paper_scissors.py
@@ -15,47 +15,71 @@ Time Complexity: O(n)
 Space Complexity: O(1)
 '''
 
-shapes = ('R', 'P', 'S')
+from enum import Enum
+
+class Shapes(Enum):
+    ROCK = 0,
+    PAPER = 1,
+    SCISSORS = 2
+
+shapes = (Shapes.ROCK, Shapes.PAPER, Shapes.SCISSORS)
 # shapes[i % 3] loses against shapes[(i+1) % 3]
 # shapes[i % 3] wins agains shapes[(i-1) % 3]
 
-def normalize_shape(shape):
-    if shape == 'A' or shape == 'X':
-        return 'R' # Rock
-    if shape == 'B' or shape == 'Y':
-        return 'P' # Paper
-    if shape == 'C' or shape == 'Z':
-        return 'S' # Scissors
-    raise Exception('Wrong input')
+shape_normalizer = {
+    'A': Shapes.ROCK,
+    'X': Shapes.ROCK,
+    'B': Shapes.PAPER,
+    'Y': Shapes.PAPER,
+    'C': Shapes.SCISSORS,
+    'Z': Shapes.SCISSORS,
+}
 
-def compute_score(opponent_shape, my_shape):
-    return _get_shape_score(my_shape) + _get_round_outcome_score(opponent_shape, my_shape)
+shape_score = {
+    Shapes.ROCK: 1, 
+    Shapes.PAPER: 2, 
+    Shapes.SCISSORS: 3
+}
 
-def _get_shape_score(shape):
-    if shape == 'R':
-        return 1
-    if shape == 'P':
-        return 2
-    if shape == 'S':
-        return 3
-    raise Exception(f"Shape not found: {shape}")
 
-def _get_round_outcome_score(opponent_shape, my_shape):
-    if opponent_shape == my_shape: # Draw
-        return 3
+class Outcome(Enum):
+    LOSE = 0,
+    DRAW = 1,
+    WIN = 2
+
+outcome_normalizer = {
+    'X': Outcome.LOSE,
+    'Y': Outcome.DRAW,
+    'Z': Outcome.WIN
+}
+
+match_outcome_score = {
+    Outcome.WIN: 6,
+    Outcome.DRAW: 3,
+    Outcome.LOSE: 0
+}
+
+
+def compute_score(opponent_shape: Shapes, my_shape: Shapes) -> int:
+    return shape_score[my_shape] + _get_round_outcome_score(opponent_shape, my_shape)
+
+def _get_round_outcome_score(opponent_shape: Shapes, my_shape: Shapes) -> int:
+    if opponent_shape == my_shape:
+        return match_outcome_score[Outcome.DRAW]
+    
     my_shape_index = shapes.index(my_shape)
     opponent_shape_index = shapes.index(opponent_shape)
-    if my_shape_index == (opponent_shape_index+1)%3: # Win
-        return 6
-    return 0 # Lose
+    if my_shape_index == (opponent_shape_index+1)%3:
+        return match_outcome_score[Outcome.WIN]
+    return match_outcome_score[Outcome.LOSE]
 
-def get_shape_based_on_round_outcome(opponent_shape, round_outcome):
+def get_shape_based_on_round_outcome(opponent_shape: Shapes, round_outcome: Outcome)-> Shapes:
     opponent_shape_index = shapes.index(opponent_shape)
-    if round_outcome == 'Y': # Draw
+    if round_outcome == Outcome.DRAW:
         my_shape = shapes[opponent_shape_index] 
-    elif round_outcome == 'X': # Lose
+    elif round_outcome == Outcome.LOSE:
         my_shape =  shapes[(opponent_shape_index-1)%3] 
-    elif round_outcome == 'Z': # Win
+    elif round_outcome == Outcome.WIN:
         my_shape = shapes[(opponent_shape_index+1)%3]
     return my_shape
 
@@ -63,8 +87,8 @@ def main_part_one():
     total_score = 0
     with open('02/input.txt', 'r') as file:
         for line in file:
-            opponent_shape = normalize_shape(line[0])
-            my_shape = normalize_shape(line[2])
+            opponent_shape = shape_normalizer[line[0]]
+            my_shape = shape_normalizer[line[2]]
             total_score += compute_score(opponent_shape, my_shape)
     print(f"[1] The total score with strategy #1 is {total_score}")
 
@@ -72,8 +96,9 @@ def main_part_two():
     total_score = 0
     with open('02/input.txt', 'r') as file:
         for line in file:
-            opponent_shape = normalize_shape(line[0])
-            my_shape = get_shape_based_on_round_outcome(opponent_shape, line[2])
+            opponent_shape = shape_normalizer[line[0]]
+            outcome = outcome_normalizer[line[2]]
+            my_shape = get_shape_based_on_round_outcome(opponent_shape, outcome)
             total_score += compute_score(opponent_shape, my_shape)
     print(f"[2] The total score with strategy #2 is {total_score}")
 
